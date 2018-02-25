1bbb411fcb6877417f29ab988e4143ca04cb402a
Fadhil Abubaker <fadhil.abubaker@gmail.com>
2018-02-25 18:39:16 +0300

    Add secondary IP to video demo

--- demos/video/laptop/video_receiver.py
+++ demos/video/laptop/video_receiver.py
@@ -8,11 +8,22 @@ import numpy as np
 # zmq port details
 PORT = "9999"
 context = zmq.Context()
-socket = context.socket(zmq.SUB)
+socket1 = context.socket(zmq.SUB)
+socket2 = context.socket(zmq.SUB)
 topic = "/video"
-#socket.connect("tcp://10.42.0.53:%s" % PORT) ## nsl-shared ip
-socket.connect("tcp://192.168.1.139:%s" % PORT) ## hive ip
-socket.setsockopt(zmq.SUBSCRIBE, topic)
+
+str_ip = ["192.168.1.139", "192.168.1.140"]
+#str_ip = ["10.42.0.53", "10.42.0.38"]
+socket_ip = {socket1: str_ip[0], socket2: str_ip[1]}
+
+#socket1.connect("tcp://192.168.1.139:%s" % PORT) ## hive ip
+
+socket1.connect("tcp://%s:%s" % (str_ip[0], PORT)) ## nsl-shared ip
+socket1.setsockopt(zmq.SUBSCRIBE, topic)
+socket2.connect("tcp://%s:%s" % (str_ip[1], PORT))
+socket2.setsockopt(zmq.SUBSCRIBE, topic)
+
+sockets = [socket1, socket2]
 
 def convertToRGB(img):
         return cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
@@ -46,22 +57,24 @@ if __name__ == "__main__":
     lbp_face_cascade = cv2.CascadeClassifier('lbpcascade_frontalface.xml')
     
     while True:
-        topic_recv = socket.recv()
-        seqno      = socket.recv()
-        image      = recv_array(socket)
+        readable, writable, exceptional = zmq.select(sockets, [], [])
+        for socket in readable:                
+                topic_recv = socket.recv()
+                seqno      = socket.recv()
+                image      = recv_array(socket)
 
-        print "Message %s received..." % seqno
+                print "Message %s received from %s" % (seqno, socket_ip[socket])
 
-        faces_detected_img = detect_faces(lbp_face_cascade, image)
-        
-        # show the frame
-        cv2.imshow("Frame", faces_detected_img)
-        key = cv2.waitKey(1) & 0xFF
+                faces_detected_img = detect_faces(lbp_face_cascade, image)
+                
+                # show the frame
+                cv2.imshow("Frame %s" % socket_ip[socket], faces_detected_img)
+                key = cv2.waitKey(1) & 0xFF
 
-        # if the `q` key was pressed, break from the loop
-        if key == ord("q"):
-            break
-
+                # if the `q` key was pressed, break from the loop
+                if key == ord("q"):
+                        break
+      
 
 
                                                         
